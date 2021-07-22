1b3c3dbd7f468cc7133e1c7fca0ae3aca1f2195d
kttkkkng <kongyour1234@gmail.com>
2021-07-23 03:07:46 +0700

    add: read json file

--- Invoker.go
+++ Invoker.go
@@ -1,9 +1,23 @@
 package workload
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Helloworld")
+	if len(os.Args) < 2 {
+		log.Fatalln("config file must be provide as first argument")
+	}
+	var workload map[string]interface{}
+	file, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = json.Unmarshal([]byte(file), &workload)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
